aeb479d78e5c83099423cb4c98873cc48ff02feb
Maxim Schoemaker <maxim.schoemaker@gmail.com>
2024-07-24 17:03:22 +0200

    created CreatePortfolioItem component, form and action

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useFormState, useFormStatus } from "react-dom";
-import { updatePortfolioItem } from "@/server/actions";
+import { createPortfolioItem, updatePortfolioItem } from "@/server/actions";
 
-import type { UpdatePortfolioItemResponse } from "@/server/actions";
 import type { PortfolioItem } from "@/server/api";
+import type { CreatePortfolioItemResponse } from "@/server/actions";
+import { useRef } from "react";
 
 type AdminProps = {
    items: PortfolioItem[];
@@ -14,17 +15,39 @@ export default function Admin({ items }: AdminProps) {
    return (
       <main>
          {items.map(item => (
-            <AdminItem key={item.id} item={item} />
+            <UpdatePortfolioItem key={item.id} item={item} />
          ))}
+         <CreatePortfolioItem />
       </main>
    );
 }
 
-type AdminItemProps = {
+function CreatePortfolioItem() {
+   const ref = useRef<HTMLFormElement>(null);
+   const submitForm = async (prevState: CreatePortfolioItemResponse, formData: FormData) => {
+      const res = await createPortfolioItem(prevState, formData);
+      ref.current?.reset();
+      return res;
+   }
+
+   const [state, dispatch] = useFormState(submitForm, { success: false, message: "" });
+
+   return (
+      <>
+         <form action={dispatch} ref={ref}>
+            <input type="text" name="title" />
+            <input type="text" name="description" />
+            <Submit text="Create" pendingMessage="Creating..." responseMessage={state.message} />
+         </form>
+      </>
+   )
+}
+
+type UpdatePortfolioItemProps = {
    item: PortfolioItem;
 }
 
-function AdminItem({ item }: AdminItemProps) {
+function UpdatePortfolioItem({ item }: UpdatePortfolioItemProps) {
    const [state, dispatch] = useFormState(updatePortfolioItem, { success: false, message: "" });
 
    return (
@@ -34,22 +57,24 @@ function AdminItem({ item }: AdminItemProps) {
             <input type="text" name="title" defaultValue={item.title} />
             <input type="text" name="description" defaultValue={item.description} />
             <input type="text" name="image" defaultValue={item.image} hidden />
-            <Submit state={state} />
+            <Submit text="Update" pendingMessage="Updating..." responseMessage={state.message} />
          </form>
       </>
    )
 }
 
 type SubmitProps = {
-   state: UpdatePortfolioItemResponse
+   text: string,
+   pendingMessage: string,
+   responseMessage: string
 }
 
-function Submit({ state }: SubmitProps) {
+function Submit({ text, pendingMessage, responseMessage }: SubmitProps) {
    const status = useFormStatus();
-   const message = status.pending ? "Submitting..." : state.message
+   const message = status.pending ? pendingMessage : responseMessage
    return (
       <>
-         <button disabled={status.pending}>Submit</button>
+         <button disabled={status.pending}>{text}</button>
          <p>{message}</p>
       </>
    );
--- src/server/actions.ts
+++ src/server/actions.ts
@@ -2,6 +2,44 @@
 
 import { revalidatePath } from 'next/cache';
 
+export type CreatePortfolioItemResponse = {
+   success: boolean,
+   message: string
+}
+
+export async function createPortfolioItem(prevState: CreatePortfolioItemResponse, formData: FormData) {
+   try {
+      const title: string | null = formData.get('title') as string;
+      const description: string | null = formData.get('description') as string;
+      const image: string | null = formData.get('image') as string;
+
+      const data = {
+         title,
+         description,
+         image
+      }
+
+      const response = await fetch(`https://${process.env.MOCK_API_KEY}.mockapi.io/api/v1/PortfolioItem/`, {
+         method: "POST",
+         headers: { 'content-type': 'application/json' },
+         body: JSON.stringify(data)
+      });
+
+      if (!response.ok) throw new Error(`Failed to create portfolio item: ${response.statusText}`);
+
+      revalidatePath('/admin');
+      revalidatePath('/');
+
+      return { success: true, message: "Successfuly created portfolio item!" }
+
+   } catch (error) {
+      console.error(error);
+      let message = 'Unknown Error'
+      if (error instanceof Error) message = error.message
+      return { success: false, message }
+   }
+}
+
 export type UpdatePortfolioItemResponse = {
    success: boolean,
    message: string
@@ -27,7 +65,7 @@ export async function updatePortfolioItem(prevState: UpdatePortfolioItemResponse
          body: JSON.stringify(data)
       });
 
-      if (!response.ok) throw new Error(`Failed to submit form: ${response.statusText}`);
+      if (!response.ok) throw new Error(`Failed to updated portfolio item: ${response.statusText}`);
 
       revalidatePath('/admin');
       revalidatePath('/');
