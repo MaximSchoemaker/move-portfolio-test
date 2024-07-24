6acc32792737d37be57ddc04f3ebbed6c1835cb2
Maxim Schoemaker <maxim.schoemaker@gmail.com>
2024-07-24 13:50:56 +0200

    put mockapi key in .env and create .env.example

--- src/server/api.ts
+++ src/server/api.ts
@@ -7,7 +7,7 @@ export type PortfolioItem = {
 };
 
 export async function getPortfolioItems() {
-   const response = await fetch("https://66a0e0407053166bcabd3601.mockapi.io/api/v1/PortfolioItem", {
+   const response = await fetch(`https://${process.env.MOCK_API_KEY}.mockapi.io/api/v1/PortfolioItem`, {
       method: "GET",
       headers: { 'content-type': 'application/json' },
    });
